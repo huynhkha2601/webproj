e4a2dc4e9fda50d1c8b2a285f0e7777339ebc8b4
NguyenQuocThong2603 <63840452+NguyenQuocThong2603@users.noreply.github.com>
2021-12-22 00:10:25 +0700

    Update data

--- public/sql/qldg_product_41-60.sql
+++ public/sql/qldg_product_41-60.sql
@@ -1,37 +1,39 @@
-INSERT INTO qldg.product (productname, title, description, type, price, step, purchaseprice, rating, dateend) VALUES ('MLB Chunky New York Monogram LT - Black Logo', 'Giày chính hãng', '', 3, 500000, 100000, 1600000, null, '2022-01-10 9:00:08');
+INSERT INTO qldg.product (productname, title, description, type, price, step, purchaseprice, rating, dateend) VALUES ('MLB Chunky New York Monogram LT - Black Logo', 'Giày chính hãng', '', 3, 500000, 100000, 1600000, null, '2022-02-10 9:00:08');
 
-INSERT INTO qldg.product (productname, title, description, type, price, step, purchaseprice, rating, dateend) VALUES ('Nike Air Force 1 - All white', 'Giày chính hãng giá sốc', '', 3, 1000000, 100000, 2500000, null, '2022-01-10 10:00:00');
+INSERT INTO qldg.product (productname, title, description, type, price, step, purchaseprice, rating, dateend) VALUES ('Nike Air Force 1 - All white', 'Giày chính hãng giá sốc', '', 3, 1000000, 100000, 2500000, null, '2022-02-10 10:00:00');
 
-INSERT INTO qldg.product (productname, title, description, type, price, step, purchaseprice, rating, dateend) VALUES ('Air Jordan 1 High - Royal Toe - White/black', 'Giày chính hãng giá sốc', '', 3, 3500000, 100000, 5900000, null, '2022-01-11 0:00:00');
+INSERT INTO qldg.product (productname, title, description, type, price, step, purchaseprice, rating, dateend) VALUES ('Air Jordan 1 High - Royal Toe - White/black', 'Giày chính hãng giá sốc', '', 3, 3500000, 100000, 5900000, null, '2022-02-11 0:00:00');
 
-INSERT INTO qldg.product (productname, title, description, type, price, step, purchaseprice, rating, dateend) VALUES ('Mule đạp gót - White/black', 'Giày chính hãng giá cực sốc', '', 3, 500000, 100000, 1500000, null, '2022-01-11 0:00:00');
+INSERT INTO qldg.product (productname, title, description, type, price, step, purchaseprice, rating, dateend) VALUES ('Mule đạp gót - White/black', 'Giày chính hãng giá cực sốc', '', 3, 500000, 100000, 1500000, null, '2022-02-11 0:00:00');
 
-INSERT INTO qldg.product (productname, title, description, type, price, step, purchaseprice, rating, dateend) VALUES ('Air Jordan 1 Mid - Triple White', 'Giày chính hãng cực hot', '', 3, 2000000, 100000, 3500000, null, '2022-01-12 0:00:00');
+INSERT INTO qldg.product (productname, title, description, type, price, step, purchaseprice, rating, dateend) VALUES ('Air Jordan 1 Mid - Triple White', 'Giày chính hãng cực hot', '', 3, 2000000, 100000, 3500000, null, '2022-02-12 0:00:00');
 
-INSERT INTO qldg.product (productname, title, description, type, price, step, purchaseprice, rating, dateend) VALUES ('Adidas Ultraboost 4.0 DNA - White/Black', 'Giày chạy bộ siêu êm', '', 3, 1000000, 100000, 2500000, null, '2022-01-10 17:00:00');
+INSERT INTO qldg.product (productname, title, description, type, price, step, purchaseprice, rating, dateend) VALUES ('Adidas Ultraboost 4.0 DNA - White/Black', 'Giày chạy bộ siêu êm', '', 3, 1000000, 100000, 2500000, null, '2022-02-10 17:00:00');
 
-INSERT INTO qldg.product (productname, title, description, type, price, step, purchaseprice, rating, dateend) VALUES ('Air Jordan 1 Low Mettalic Gold - White/Gold', 'Giày chính hãng', '', 3, 2000000, 100000, 4800000, null, '2022-01-10 18:00:00');
+INSERT INTO qldg.product (productname, title, description, type, price, step, purchaseprice, rating, dateend) VALUES ('Air Jordan 1 Low Mettalic Gold - White/Gold', 'Giày chính hãng', '', 3, 2000000, 100000, 4800000, null, '2022-02-10 18:00:00');
 
-INSERT INTO qldg.product (productname, title, description, type, price, step, purchaseprice, rating, dateend) VALUES ('Adidas SuperStar Gold - White/Gold logo', 'Giày adidas chính hãng', '', 3, 500000, 100000, 1500000, null, '2022-01-11 18:00:00');
+INSERT INTO qldg.product (productname, title, description, type, price, step, purchaseprice, rating, dateend) VALUES ('Adidas SuperStar Gold - White/Gold logo', 'Giày adidas chính hãng', '', 3, 500000, 100000, 1500000, null, '2022-02-11 18:00:00');
 
-INSERT INTO qldg.product (productname, title, description, type, price, step, purchaseprice, rating, dateend) VALUES ('MLB Chunky LT New York Yankees trắng - White', 'Giày chính hãng', '', 3, 500000, 100000, 1800000, null, '2022-01-11 20:00:00');
+INSERT INTO qldg.product (productname, title, description, type, price, step, purchaseprice, rating, dateend) VALUES ('MLB Chunky LT New York Yankees trắng - White', 'Giày chính hãng', '', 3, 500000, 100000, 1800000, null, '2022-02-11 20:00:00');
 
-INSERT INTO qldg.product (productname, title, description, type, price, step, purchaseprice, rating, dateend) VALUES ('Adidas Stansmith xanh navy - White/Navy', 'Giày adidas chính hãng', '', 3, 500000, 100000, 1500000, null, '2022-01-10 22:00:00');
+INSERT INTO qldg.product (productname, title, description, type, price, step, purchaseprice, rating, dateend) VALUES ('Adidas Stansmith xanh navy - White/Navy', 'Giày adidas chính hãng', '', 3, 500000, 100000, 1500000, null, '2022-02-10 22:00:00');
 
-INSERT INTO qldg.product (productname, title, description, type, price, step, purchaseprice, rating, dateend) VALUES ('Air Jordan 1 Mid UNC - White/Carolina blue', 'Giày chính hãng hot', '', 3, 3000000, 100000, 5800000, null, '2022-01-12 22:00:00');
+INSERT INTO qldg.product (productname, title, description, type, price, step, purchaseprice, rating, dateend) VALUES ('Air Jordan 1 Mid UNC - White/Carolina blue', 'Giày chính hãng hot', '', 3, 3000000, 100000, 5800000, null, '2022-02-12 22:00:00');
 
-INSERT INTO qldg.product (productname, title, description, type, price, step, purchaseprice, rating, dateend) VALUES ('JORDAN 1 HIGH ZOOM AIR CMFT PSG PARIS SAINT-GERMAIN', 'Giày chính hãng hot', '', 3, 5000000, 100000, 8500000, null, '2022-01-12 00:00:00');
+INSERT INTO qldg.product (productname, title, description, type, price, step, purchaseprice, rating, dateend) VALUES ('JORDAN 1 HIGH ZOOM AIR CMFT PSG PARIS SAINT-GERMAIN', 'Giày chính hãng hot', '', 3, 5000000, 100000, 8500000, null, '2022-02-12 00:00:00');
 
-INSERT INTO qldg.product (productname, title, description, type, price, step, purchaseprice, rating, dateend) VALUES ('JORDAN 1 MID SE UNION BLACK TOE', 'Giày chính hãng cao cấp', '', 3, 1000000, 100000, 4900000, null, '2022-01-10 00:00:00');
+INSERT INTO qldg.product (productname, title, description, type, price, step, purchaseprice, rating, dateend) VALUES ('XVESSEL SNEAKER BLACK', 'Giày chính hãng cao cấp', '', 3, 3000000, 100000, 8000000, null, '2022-02-10 00:00:00');
 
-INSERT INTO qldg.product (productname, title, description, type, price, step, purchaseprice, rating, dateend) VALUES ('NIKE AIR FORCE 1 LOW G-DRAGON PEACEMINUSONE PARA-NOISE', 'Giày chính hãng cao cấp', '', 3, 12000000, 100000, 21000000, null, '2022-01-13 00:00:00');
+INSERT INTO qldg.product (productname, title, description, type, price, step, purchaseprice, rating, dateend) VALUES ('JORDAN 1 MID SE UNION BLACK TOE', 'Giày chính hãng cao cấp', '', 3, 1000000, 100000, 4900000, null, '2022-02-10 00:00:00');
 
-INSERT INTO qldg.product (productname, title, description, type, price, step, purchaseprice, rating, dateend) VALUES ('GUCCI RHYTON LOGO LEATHER SNEAKER', 'Giày cao cấp', '', 3, 10000000, 100000, 19000000, null, '2022-01-14 18:00:00');
+INSERT INTO qldg.product (productname, title, description, type, price, step, purchaseprice, rating, dateend) VALUES ('NIKE AIR FORCE 1 LOW G-DRAGON PEACEMINUSONE PARA-NOISE', 'Giày chính hãng cao cấp', '', 3, 12000000, 100000, 21000000, null, '2022-02-13 00:00:00');
 
-INSERT INTO qldg.product (productname, title, description, type, price, step, purchaseprice, rating, dateend) VALUES ('GUCCI X Disney Donald Duck Ace Sneaker', 'Giày cao cấp', '', 3, 5000000, 100000, 13500000, null, '2022-01-14 22:00:00');
+INSERT INTO qldg.product (productname, title, description, type, price, step, purchaseprice, rating, dateend) VALUES ('GUCCI RHYTON LOGO LEATHER SNEAKER', 'Giày cao cấp', '', 3, 10000000, 100000, 19000000, null, '2022-02-14 18:00:00');
 
-INSERT INTO qldg.product (productname, title, description, type, price, step, purchaseprice, rating, dateend) VALUES ('DIOR B23 WHITE AND WHITE SNEAKER', 'Giày cao cấp chính hãng', '', 3, 12000000, 100000, 24000000, null, '2022-01-12 22:00:00');
+INSERT INTO qldg.product (productname, title, description, type, price, step, purchaseprice, rating, dateend) VALUES ('GUCCI X Disney Donald Duck Ace Sneaker', 'Giày cao cấp', '', 3, 5000000, 100000, 13500000, null, '2022-02-14 22:00:00');
 
-INSERT INTO qldg.product (productname, title, description, type, price, step, purchaseprice, rating, dateend) VALUES ('Adidas Pharrell X NMD Trail 'Human Race'', 'Giày cao cấp chính hãng', '', 3, 3000000, 100000, 6000000, null, '2022-01-10 22:00:00');
+INSERT INTO qldg.product (productname, title, description, type, price, step, purchaseprice, rating, dateend) VALUES ('DIOR B23 WHITE AND WHITE SNEAKER', 'Giày cao cấp chính hãng', '', 3, 12000000, 100000, 24000000, null, '2022-02-12 22:00:00');
 
-INSERT INTO qldg.product (productname, title, description, type, price, step, purchaseprice, rating, dateend) VALUES ('Balenciaga Speed Trainer 'Clear Sole - Black', 'Giày cao cấp chính hãng', '', 3, 8000000, 100000, 15000000, null, '2022-01-12 18:00:00');
+INSERT INTO qldg.product (productname, title, description, type, price, step, purchaseprice, rating, dateend) VALUES ('Adidas Pharrell X NMD Trail ''Human Race''', 'Giày cao cấp chính hãng', '', 3, 3000000, 100000, 6000000, null, '2022-02-10 22:00:00');
+
+INSERT INTO qldg.product (productname, title, description, type, price, step, purchaseprice, rating, dateend) VALUES ('Balenciaga Speed Trainer ''Clear Sole - Black''', 'Giày cao cấp chính hãng', '', 3, 8000000, 100000, 15000000, null, '2022-02-12 18:00:00');
---
+++ public/sql/qldg_product_61-80.sql
@@ -0,0 +1,40 @@
+INSERT INTO qldg.product (productname, title, description, type, price, step, purchaseprice, rating, dateend) VALUES ('LEVENTS "MY ANIMALS" SERIES SNAKE SWEATER  / CREAM', 'Áo sweater đẹp', '', 4, 500000, 100000, 1500000, null, '2022-01-15 9:00:00');
+
+INSERT INTO qldg.product (productname, title, description, type, price, step, purchaseprice, rating, dateend) VALUES ('LEVENTS CAPSULE HOODIE/ ALL BLACK', 'Áo nam cực rẻ', '', 4, 200000, 100000, 1000000, null, '2022-01-13 0:00:00');
+
+INSERT INTO qldg.product (productname, title, description, type, price, step, purchaseprice, rating, dateend) VALUES ('Nike Sportswear Tech Fleece', 'Áo nam đẹp', '', 4, 500000, 100000, 2200000, null, '2022-01-14 12:00:00');
+
+INSERT INTO qldg.product (productname, title, description, type, price, step, purchaseprice, rating, dateend) VALUES ('Nike Sportswear Storm-FIT Windrunner', 'Áo khoác nam hot', '', 4, 500000, 100000, 2300000, null, '2022-01-14 18:00:00');
+
+INSERT INTO qldg.product (productname, title, description, type, price, step, purchaseprice, rating, dateend) VALUES ('RUNNER TEE', 'Áo nữ đẹp', '', 4, 200000, 100000, 800000, null, '2022-01-14 19:00:00');
+
+INSERT INTO qldg.product (productname, title, description, type, price, step, purchaseprice, rating, dateend) VALUES ('ADICOLOR SHATTERED TREFOIL TEE DRESS', 'Áo adidas nữ siêu đẹp', '', 4, 200000, 100000, 950000, null, '2022-01-14 20:00:00');
+
+INSERT INTO qldg.product (productname, title, description, type, price, step, purchaseprice, rating, dateend) VALUES ('LEVENTS EMGO VARSITY / RED', 'Áo khoác unisex', '', 4, 200000, 100000, 1000000, null, '2022-01-14 22:00:00');
+
+INSERT INTO qldg.product (productname, title, description, type, price, step, purchaseprice, rating, dateend) VALUES ('ADIDAS DRESS', 'Váy nữ hot', '', 4, 400000, 100000, 1600000, null, '2022-01-14 22:00:00');
+
+INSERT INTO qldg.product (productname, title, description, type, price, step, purchaseprice, rating, dateend) VALUES ('ADIDAS RETRO LUXURY TEE', 'Áo Adidas nữ cực rẻ', '', 4, 200000, 100000, 900000, null, '2022-01-14 00:00:00');
+
+INSERT INTO qldg.product (productname, title, description, type, price, step, purchaseprice, rating, dateend) VALUES ('LeBron Men Protect Jacket', 'Áo khoác nam Nike', '', 4, 300000, 100000, 1800000, null, '2022-01-12 00:00:00');
+
+INSERT INTO qldg.product (productname, title, description, type, price, step, purchaseprice, rating, dateend) VALUES ('Nike Sportswear Tech Fleece', 'Quần nam giá rẻ', '', 4, 700000, 100000, 2300000, null, '2022-01-13 16:00:00');
+
+INSERT INTO qldg.product (productname, title, description, type, price, step, purchaseprice, rating, dateend) VALUES ('LÌ VEN ESSENTIAL SHORTPANT/ GREY', 'Quần short nam đẹp', '', 4, 200000, 100000, 800000, null, '2022-01-13 18:00:00');
+
+INSERT INTO qldg.product (productname, title, description, type, price, step, purchaseprice, rating, dateend) VALUES ('ADICOLOR SHATTERED TREFOIL TRACK PANTS', 'Quần thể thao nữ đẹp', '', 4, 500000, 100000, 2200000, null, '2022-01-13 22:00:00');
+
+INSERT INTO qldg.product (productname, title, description, type, price, step, purchaseprice, rating, dateend) VALUES ('Nike Therma', 'Quần thể thao nam cực hot', '', 4, 500000, 100000, 1500000, null, '2022-01-13 23:00:00');
+
+INSERT INTO qldg.product (productname, title, description, type, price, step, purchaseprice, rating, dateend) VALUES ('ADIDAS RETRO LUXURY SWEAT PANTS', 'Quần thể thao adidas nữ', '', 4, 400000, 100000, 1600000, null, '2022-01-15 23:00:00');
+
+INSERT INTO qldg.product (productname, title, description, type, price, step, purchaseprice, rating, dateend) VALUES ('Nike Sportswear Club Fleece', 'Quần nike cực đẹp', '', 4, 400000, 100000, 1600000, null, '2022-01-16 20:00:00');
+
+INSERT INTO qldg.product (productname, title, description, type, price, step, purchaseprice, rating, dateend) VALUES ('AEROREADY FOUR-WAY STRETCH PANTS', 'Quần dài nữ cực xinh', '', 4, 700000, 100000, 2500000, null, '2022-01-17 20:00:00');
+
+INSERT INTO qldg.product (productname, title, description, type, price, step, purchaseprice,rating, dateend) VALUES ('Nike Sportswear', 'Quần short thể thao nam', '', 4, 300000, 100000, 1200000, null, '2022-01-13 22:00:00');
+
+INSERT INTO qldg.product (productname, title, description, type, price, step, purchaseprice,rating, dateend) VALUES ('ESSENTIALS FRENCH TERRY 3-STRIPES PANTS', 'Quần thể thao nữ hot', '', 4, 300000, 100000, 1200000, null, '2022-01-13 00:00:00');
+
+INSERT INTO qldg.product (productname, title, description, type, price, step, purchaseprice,rating, dateend) VALUES ('FUTURE ICONS PANTS', 'Quần adidas nam', '', 4, 300000, 100000, 1500000, null, '2022-01-13 00:00:00');
+
